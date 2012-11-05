1c0b8b50b8e3bd814fd6280bd981e883d151b54e
Stephen Lujan <slujan@catalystitservices.com>
2012-11-05 11:21:55 -0500

    fixed file structure to better suit a library

--- example/demo.dart
+++ example/demo.dart
@@ -1,12 +1,11 @@
 import 'dart:html';
-import 'presentation.dart';
+import 'package:lib/presentation.dart';
 
 num rotatePos = 0;
 
 void main() {
   var presentation = new Presentation(query("#render"), query("#scene"));
   
-  /*
   presentation.addHtmlSlide("Slide1", 1.0, 0.0,    0.0,    20000.0,   0.0,0.0,0.0);
   presentation.addHtmlSlide("Slide2", 1.0, -300.0, 0.0,    0.0,     0.0,0.0,0.0);
   presentation.addHtmlSlide("Slide3", 1.0, 300.0,  0.0,    0.0,     0.0,0.0,0.0);
@@ -16,13 +15,13 @@ void main() {
   presentation.addHtmlSlide("Slide7", 1.0, -700.0, -150.0, 150.0,   0.0,0.0,90.0);
   presentation.addHtmlSlide("Slide8", 1.0, -100.0,  100.0, 150.0,   70.0,0.0,0.0);
   presentation.addHtmlSlide("Slide9", 1.0, 150.0,  -300.0, 300.0,   0.0,70.0,0.0);
-  */
   
+  /*
   presentation.addHtmlSlide("Slide1", 1.0, -300.0,  -300.0, -300.0,  0.0,0.0,0.0);
   presentation.addHtmlSlide("Slide2", 1.0, -300.0,  -300.0, -300.0,  90.0,0.0,0.0);
   presentation.addHtmlSlide("Slide3", 1.0, -300.0,  -300.0, -300.0,  0.0,90.0,0.0);
   presentation.addHtmlSlide("Slide4", 1.0, -300.0,  -300.0, -300.0, 90.0,90.0,90.0);
- 
+  */
 
   // Handle key events.
   document.on.keyDown.add((KeyboardEvent event) {
